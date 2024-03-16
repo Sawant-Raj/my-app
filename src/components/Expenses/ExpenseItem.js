09956602c23fd8c4631f2f5ecf94842edf9b9b9d
Sawant-Raj <2019kuec2028@iiitkota.ac.in>
2024-03-16 18:34:58 +0530

    Change Expense Button

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
+  const [price, setPrice] = useState(props.price);
+
+  const clickHandler = () => {
+    setPrice("100");
+  };
+
   return (
     <Card className="expense-item">
-      <ExpenseDate date={props.date}/>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__location">{props.location}</div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.price}</div>
+        <div className="expense-item__price">${price}</div>
       </div>
-      <button>Delete Expense</button>
+      <button onClick={clickHandler}>Change Expense</button>
     </Card>
   );
 };
